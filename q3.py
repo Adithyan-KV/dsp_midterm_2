1f7393297a81a3c3091a01b92a942db1301ab507
adithyan-kv <adithyankv007@gmail.com>
2021-01-11 23:11:14 +0530

    Final version

--- q3.py
+++ q3.py
@@ -20,17 +20,17 @@ def main():
 
 
 def get_ratios(x_8, x_16, x_32, freq_low, freq_high):
-    power_8 = fft.fft(x_8) * np.conj(fft.fft(x_8))
-    power_16 = fft.fft(x_16) * np.conj(fft.fft(x_16))
-    power_32 = fft.fft(x_32) * np.conj(fft.fft(x_32))
+    psd_8 = fft.fft(x_8) * np.conj(fft.fft(x_8))
+    psd_16 = fft.fft(x_16) * np.conj(fft.fft(x_16))
+    psd_32 = fft.fft(x_32) * np.conj(fft.fft(x_32))
 
-    E_8 = np.sum(power_8)
-    E_16 = np.sum(power_16)
-    E_32 = np.sum(power_32)
+    E_8 = np.sum(psd_8)
+    E_16 = np.sum(psd_16)
+    E_32 = np.sum(psd_32)
 
-    p_8 = power_8[freq_low * 1000:freq_high * 1000].sum()
-    p_16 = power_16[freq_low * 1000:freq_high * 1000].sum()
-    p_32 = power_32[freq_low * 1000:freq_high * 1000].sum()
+    p_8 = psd_8[freq_low * 1000:freq_high * 1000].sum()
+    p_16 = psd_16[freq_low * 1000:freq_high * 1000].sum()
+    p_32 = psd_32[freq_low * 1000:freq_high * 1000].sum()
 
     print(f"Frequency range{freq_low}-{freq_high}")
     print(f"E_8/p_8:{E_8 / p_8}")
